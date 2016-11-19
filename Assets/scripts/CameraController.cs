38db38b79d76c92ac9bda168b86dbb5d500c37ac
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-19 19:02:42 +0000

    Add camera rotation to current player

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CameraController : MonoBehaviour {
+
+	public void RotateToCurrentPlayer () {
+		
+		transform.DORotate(new Vector3(0, 0,CalculateZRotationForCurrentPlayer()), 1.5f);
+	}
+
+	private float CalculateZRotationForCurrentPlayer () {
+		int numberOfPlayers = GameManager.Instance.NumberOfPlayers;
+		int currentPlayer = GameManager.Instance.CurrentPlayer;
+
+		float relativePlayerPosition = (float)currentPlayer / (float)(numberOfPlayers - 1);
+		return relativePlayerPosition * 180 - 90;
+	}
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance;
+
+	private Camera mainCam;
+
+	private int numberOfPlayers = 3; // TODO - Hardcoded, to beread from menu selection
+	public int NumberOfPlayers {
+		get {
+			return numberOfPlayers;
+		}
+	}
+
+	private int currentPlayer = 1;
+	public int CurrentPlayer {
+		get {
+			return currentPlayer;
+		}
+	}
+
+	private void Awake () {
+		MakeSingleton ();
+		mainCam = Camera.main;
+	}
+
+	// TODO - Remove when finished testing
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.N)) {
+			MoveToNextPlayer();
+		}
+	}
+
+	private void MakeSingleton () {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (Instance);
+		} else {
+			DestroyImmediate (gameObject);
+		}
+	}
+
+	private void MoveToNextPlayer () {
+		// Move to next player
+		currentPlayer++;
+
+		// If we were at the last player, move to first player
+		if (currentPlayer >= numberOfPlayers) {
+			currentPlayer = 0;
+		}
+		mainCam.GetComponent<CameraController> ().RotateToCurrentPlayer ();
+	}
+}
