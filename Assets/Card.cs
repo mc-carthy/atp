81b9ab2358ff4db0ea088c6603afedd5a12183e6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-11-19 18:42:04 +0000

    Add DrawCardFromDrawDeck method to draw a card from the deck to the player's hand

---
+++ Assets/Card.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Card : MonoBehaviour {
+
+	public void MoveToPosition (Vector3 position, Vector3 rotation, float moveTime) {
+		transform.DOMove (position, moveTime, false);
+		transform.DORotate (rotation, moveTime);
+	}
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Player : MonoBehaviour {
+
+	public Card cardToDraw;
+
+	private void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			DrawCardFromDrawDeck();
+		}
+	}
+
+	private void DrawCardFromDrawDeck () {
+		cardToDraw.MoveToPosition(transform.position, transform.localRotation.eulerAngles, 1.5f);
+	}
+}
